061c5873e3b4deebf18e283d34a7f419b3f23d18
luiz-educosta <luizeduardorodrigues@tai.inatel>
2020-10-14 18:47:46 -0300

    Adicionando programa do curso da Alura

---
+++ adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    
+    //imprime cabeçalho do jogo
+    printf("***************************************\n");
+    printf("*Bem vindo ao nosso jogo de advinhação*\n");
+    printf("***************************************\n");
+}
