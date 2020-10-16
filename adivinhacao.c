fccd9e7d4d001f926d7fbc4f0e2e4b89ef70ac86
luiz-educosta <luizeduardorodrigues@tai.inatel>
2020-10-15 22:45:43 -0300

    Atualizando o jogo de adivinhacao 15_10

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define TENTATIVAS 3
+
 int main() {
     
     //imprime cabeçalho do jogo
@@ -10,25 +12,51 @@ int main() {
     //declaração de variaveis
     int numerosecreto = 42;
     int chute;
+    int i;
 
-    //leitura do número chutado
-    printf("Qual é o seu chute?\n");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d. \n", chute);
 
-    //tomando a decisão se acertou ou não
-    if(chute == numerosecreto){
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
-    }
-    else{
-        if(chute > numerosecreto){
-            printf("Seu chute foi maior que o número secreto!\n");
+    //loop de repetição até ter 3 acertos
+    for (i = 1; i <= TENTATIVAS; i++){
+
+
+        printf("Tentativa %d de %d!\n",i,TENTATIVAS);
+
+        //leitura do número chutado
+        printf("Qual é o seu chute?\n");
+         scanf("%d", &chute);
+        printf("Seu chute foi %d. \n", chute);
+
+        //tomando a decisão se acertou ou não
+        int acertou = (chute == numerosecreto);
+        if(acertou){
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+
+            //sai do loop
+            break;
+         }
+            else{
+
+            int maior = chute > numerosecreto;
+
+            if(maior){
+                printf("Seu chute foi maior que o número secreto!\n");
+            } else{
+                printf("Seu chute foi menor que o número secreto!\n");
         }
-        if(chute < numerosecreto){
-            printf("Seu chute foi menor que o número secreto!\n");
-        }
+        
+            
     }
+
+       
+     printf("Fim de jogo!\n");
+
+
+
+    }
+
+
+    
     
 
 }
